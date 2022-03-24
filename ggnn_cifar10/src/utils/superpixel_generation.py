caf16075d95cec6868fd5572675c00c247acacbe
yangqingchen98 <76038042+yangqingchen98@users.noreply.github.com>
2022-03-24 21:36:42 +0100

    Add files via upload

---
+++ ggnn_cifar10/src/utils/superpixel_generation.py
@@ -0,0 +1,115 @@
+import multiprocessing
+import os
+import time
+from itertools import repeat
+
+import networkx as nx
+import numpy as np
+import torch
+from skimage.segmentation import slic
+from torch_geometric.data import Data
+
+from tqdm.auto import tqdm
+
+from src.utils.multiprocessing_istarmap import multiprocessing_istarmap
+
+
+def convert_img_dataset_to_superpixel_graph_dataset(
+    images, labels, desired_nodes: int = 75, num_workers: int = 4, **slic_kwargs
+):
+
+    assert len(images) == len(labels)
+
+    data_list = []
+
+    start_time = time.time()
+
+
+    multiprocessing.pool.Pool.istarmap = multiprocessing_istarmap
+
+    with multiprocessing.Pool(num_workers) as pool:
+        args = list(zip(images, labels, repeat(desired_nodes), repeat(slic_kwargs)))
+
+        for graph in tqdm(
+            pool.istarmap(convert_numpy_img_to_superpixel_graph, args),
+            total=len(args),
+            desc="Generating superpixels",
+            colour="GREEN",
+        ):
+            x, edge_index, pos, y = graph
+            x = torch.as_tensor(x, dtype=torch.float32)
+            edge_index = torch.as_tensor(edge_index, dtype=torch.long).T
+            pos = torch.as_tensor(pos, dtype=torch.float32)
+            y = torch.as_tensor(y, dtype=torch.long)
+            data_list.append(Data(x=x, edge_index=edge_index, pos=pos, y=y))
+
+    total_time = time.time() - start_time
+    print(f"Took {total_time}s.")
+
+    return data_list
+
+
+def convert_numpy_img_to_superpixel_graph(img, label, desired_nodes: int = 75, slic_kwargs={}):
+
+    img = img / 255
+
+    height = img.shape[0]
+    width = img.shape[1]
+    num_of_features = img.shape[2]
+
+    segments = slic(img, n_segments=desired_nodes, slic_zero=True, start_label=0, **slic_kwargs)
+
+    num_of_nodes = np.max(segments) + 1
+    nodes = {node: {"rgb_list": [], "pos_list": []} for node in range(num_of_nodes)}
+
+    for y in range(height):
+        for x in range(width):
+            node = segments[y, x]
+
+            rgb = img[y, x, :]
+            nodes[node]["rgb_list"].append(rgb)
+
+            pos = np.array([float(x) / width, float(y) / height])
+            nodes[node]["pos_list"].append(pos)
+
+    G = nx.Graph()
+    for node in nodes:
+        nodes[node]["rgb_list"] = np.stack(nodes[node]["rgb_list"])
+        nodes[node]["pos_list"] = np.stack(nodes[node]["pos_list"])
+        rgb_mean = np.mean(nodes[node]["rgb_list"], axis=0)
+        G.add_node(node, features=list(rgb_mean))
+
+    # compute node positions
+    segments_ids = np.unique(segments)
+    pos = np.array([np.mean(np.nonzero(segments == i), axis=1) for i in segments_ids])
+    pos = pos.astype(int)
+
+    # add edges
+    vs_right = np.vstack([segments[:, :-1].ravel(), segments[:, 1:].ravel()])
+    vs_below = np.vstack([segments[:-1, :].ravel(), segments[1:, :].ravel()])
+    bneighbors = np.unique(np.hstack([vs_right, vs_below]), axis=1)
+    for i in range(bneighbors.shape[1]):
+        if bneighbors[0, i] != bneighbors[1, i]:
+            G.add_edge(bneighbors[0, i], bneighbors[1, i])
+
+    # add self loops
+    for node in nodes:
+        G.add_edge(node, node)
+
+    # get edge_index
+    m = len(G.edges)
+    edge_index = np.zeros([2 * m, 2]).astype(np.int64)
+    for e, (s, t) in enumerate(G.edges):
+        edge_index[e, 0] = s
+        edge_index[e, 1] = t
+        edge_index[m + e, 0] = t
+        edge_index[m + e, 1] = s
+
+    # get features
+    x = np.zeros([num_of_nodes, num_of_features]).astype(np.float32)
+    for node in G.nodes:
+        x[node, :] = G.nodes[node]["features"]
+
+    y = label
+
+    return x, edge_index, pos, y
---
+++ ggnn_cifar10/src/utils/utils.py
@@ -0,0 +1,148 @@
+import logging
+import warnings
+from typing import List, Sequence
+
+import pytorch_lightning as pl
+import rich.syntax
+import rich.tree
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning.utilities import rank_zero_only
+
+
+def get_logger(name=__name__) -> logging.Logger:
+    """Initializes multi-GPU-friendly python command line logger."""
+
+    logger = logging.getLogger(name)
+
+
+    for level in (
+        "debug",
+        "info",
+        "warning",
+        "error",
+        "exception",
+        "fatal",
+        "critical",
+    ):
+        setattr(logger, level, rank_zero_only(getattr(logger, level)))
+
+    return logger
+
+
+def extras(config: DictConfig) -> None:
+
+    log = get_logger(__name__)
+
+    # disable python warnings if <config.ignore_warnings=True>
+    if config.get("ignore_warnings"):
+        log.info("Disabling python warnings! <config.ignore_warnings=True>")
+        warnings.filterwarnings("ignore")
+
+    # verify experiment name is set when running in experiment mode
+    if config.get("experiment_mode") and not config.get("name"):
+        log.info(
+            "Running in experiment mode without the experiment name specified! "
+            "Use `python run.py mode=exp name=experiment_name`"
+        )
+        log.info("Exiting...")
+        exit()
+
+
+    if config.trainer.get("fast_dev_run"):
+        log.info("Forcing debugger friendly configuration! <config.trainer.fast_dev_run=True>")
+        if config.trainer.get("gpus"):
+            config.trainer.gpus = 0
+        if config.datamodule.get("pin_memory"):
+            config.datamodule.pin_memory = False
+        if config.datamodule.get("num_workers"):
+            config.datamodule.num_workers = 0
+
+
+@rank_zero_only
+def print_config(
+    config: DictConfig,
+    fields: Sequence[str] = (
+        "trainer",
+        "model",
+        "datamodule",
+        "callbacks",
+        "logger",
+        "test_after_training",
+        "seed",
+        "name",
+    ),
+    resolve: bool = True,
+) -> None:
+
+
+    style = "dim"
+    tree = rich.tree.Tree("CONFIG", style=style, guide_style=style)
+
+    for field in fields:
+        branch = tree.add(field, style=style, guide_style=style)
+
+        config_section = config.get(field)
+        branch_content = str(config_section)
+        if isinstance(config_section, DictConfig):
+            branch_content = OmegaConf.to_yaml(config_section, resolve=resolve)
+
+        branch.add(rich.syntax.Syntax(branch_content, "yaml"))
+
+    rich.print(tree)
+
+    with open("config_tree.txt", "w") as fp:
+        rich.print(tree, file=fp)
+
+
+@rank_zero_only
+def log_hyperparameters(
+    config: DictConfig,
+    model: pl.LightningModule,
+    datamodule: pl.LightningDataModule,
+    trainer: pl.Trainer,
+    callbacks: List[pl.Callback],
+    logger: List[pl.loggers.LightningLoggerBase],
+) -> None:
+
+
+    hparams = {}
+
+    # choose which parts of hydra config will be saved to loggers
+    hparams["trainer"] = config["trainer"]
+    hparams["model"] = config["model"]
+    hparams["datamodule"] = config["datamodule"]
+
+    if "seed" in config:
+        hparams["seed"] = config["seed"]
+    if "callbacks" in config:
+        hparams["callbacks"] = config["callbacks"]
+
+    # save number of model parameters
+    hparams["model/params_total"] = sum(p.numel() for p in model.parameters())
+    hparams["model/params_trainable"] = sum(
+        p.numel() for p in model.parameters() if p.requires_grad
+    )
+    hparams["model/params_not_trainable"] = sum(
+        p.numel() for p in model.parameters() if not p.requires_grad
+    )
+
+    # send hparams to all loggers
+    trainer.logger.log_hyperparams(hparams)
+
+
+def finish(
+    config: DictConfig,
+    model: pl.LightningModule,
+    datamodule: pl.LightningDataModule,
+    trainer: pl.Trainer,
+    callbacks: List[pl.Callback],
+    logger: List[pl.loggers.LightningLoggerBase],
+) -> None:
+    """Makes sure everything closed properly."""
+
+    # without this sweeps with wandb logger might crash!
+    for lg in logger:
+        if isinstance(lg, pl.loggers.wandb.WandbLogger):
+            import wandb
+
+            wandb.finish()
